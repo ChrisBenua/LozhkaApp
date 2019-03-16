f8a35b8e914e94008df85dc47d8bcc2f5d196654
ChrisBenua <kpbenua@edu.hse.ru>
2019-03-16 12:17:18 +0300

    InitialCommit

---
+++ LozhkaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LozhkaApp
+//
+//  Created by Ирина Улитина on 16/03/2019.
+//  Copyright © 2019 Christian Benua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
